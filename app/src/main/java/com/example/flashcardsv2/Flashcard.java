7493859d3fc3036aa157a559ab99142c97573b0d
tmearns <65202387+TacoMoney03@users.noreply.github.com>
2020-06-04 11:04:05 -0400

    add second card, add lots of comments

--- app/src/main/java/com/example/flashcardsv2/Flashcard.java
+++ app/src/main/java/com/example/flashcardsv2/Flashcard.java
@@ -4,34 +4,55 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 import android.widget.CompoundButton;
 import android.widget.Switch;
 import android.widget.TextView;
 
 public class Flashcard extends AppCompatActivity {
 
-    private static final String TAG = "DisplayQuestion";
-    public static final String ANSWER = "com.example.flashme.ANSWER2";
-    private static final String mypreference = "mypref";
-    private static final String question = "saveQuestion";
-    private static final String answer = "saveAnswer";
+    // declare all variables
     Intent _intent;
     private String q;
     private String a;
+    private String q2;
+    private String a2;
     Switch sw1;
+    Switch sw2;
     TextView flashcard;
+    TextView flashcard2;
+    Button next;
+    Button previous;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_flashcard);
+        // grab all views by ID on create
         sw1 = findViewById(R.id.switch1);
+        sw2 = findViewById(R.id.switch2);
         flashcard = findViewById(R.id.Flashcard);
+        flashcard2 = findViewById(R.id.Flashcard2);
+        next = findViewById(R.id.next);
+        previous = findViewById(R.id.previous);
+        // get the intent
         _intent = getIntent();
+        // get all 4 strings and set them to variables
         q = _intent.getStringExtra(MainActivity.QUESTION);
         a = _intent.getStringExtra(MainActivity.ANSWER);
+        q2 = _intent.getStringExtra(MainActivity.QUESTION2);
+        a2 = _intent.getStringExtra(MainActivity.ANSWER2);
+        // set the two flashcards to start by showing the question
         flashcard.setText(q);
+        flashcard2.setText(q2);
+        // set the second flashcard stuff to be invisible on create
+        sw2.setVisibility(View.INVISIBLE);
+        flashcard2.setVisibility(View.INVISIBLE);
+        // set the previous button to be invisible on create, since there isn't a previous card to go to at this point
+        previous.setVisibility(View.INVISIBLE);
 
+        // if statement to switch the string shown in the text view based on position of the toggle switch
         sw1.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
             public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
                 String str1;
@@ -42,5 +63,43 @@ public class Flashcard extends AppCompatActivity {
                 flashcard.setText(str1);
             }
         });
+
+        // same thing, but for the second switch, this starts out hidden
+        sw2.setOnCheckedChangeListener(new CompoundButton.OnCheckedChangeListener() {
+            public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+                String str1;
+                if (sw2.isChecked())
+                    str1 = a2;
+                else
+                    str1 = q2;
+                flashcard2.setText(str1);
+            }
+        });
+    }
+
+    // method for the next button
+    public void next(View view) {
+        // set everything for card 1 invisible, and card 2 visible
+        flashcard.setVisibility(View.INVISIBLE);
+        sw1.setVisibility(View.INVISIBLE);
+        flashcard2.setVisibility(View.VISIBLE);
+        sw2.setVisibility(View.VISIBLE);
+        // set previous button to visible since we now have something to go back to
+        previous.setVisibility(View.VISIBLE);
+        // set next button invisible since there isn't another card
+        next.setVisibility(View.INVISIBLE);
+    }
+
+    // method for the previous button
+    public void previous(View view) {
+        // set everything for card 2 invisible, and card 1 visible
+        flashcard2.setVisibility(View.INVISIBLE);
+        sw2.setVisibility(View.INVISIBLE);
+        flashcard.setVisibility(View.VISIBLE);
+        sw1.setVisibility(View.VISIBLE);
+        // hide the previous button, since we are at the start again
+        previous.setVisibility(View.INVISIBLE);
+        // make the next button visible again, since there are more cards to flip through now
+        next.setVisibility(View.VISIBLE);
     }
 }
--- app/src/main/java/com/example/flashcardsv2/MainActivity.java
+++ app/src/main/java/com/example/flashcardsv2/MainActivity.java
@@ -12,30 +12,47 @@ import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    // declare variables for preferences, logging, strings, and text views
     public static final String QUESTION = "com.example.flashcardsv2.QUESTION";
     public static final String ANSWER = "com.example.flashcardsv2.ANSWER";
+    public static final String QUESTION2 = "com.example.flashcardsv2.QUESTION2";
+    public static final String ANSWER2 = "com.example.flashcardsv2.ANSWER2";
     private static final String TAG = "MainActivity";
     private static final String mypreference = "mypref";
     private static final String question = "saveQuestion";
     private static final String answer = "saveAnswer";
+    private static final String question2 = "saveQuestion2";
+    private static final String answer2 = "saveAnswer2";
     private String q;
     private String a;
+    private String q2;
+    private String a2;
     EditText flashcardQuestion;
     EditText flashcardAnswer;
+    EditText flashcardQuestion2;
+    EditText flashcardAnswer2;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        // grab the 4 views by ID on create - keeps later methods cleaner
         flashcardQuestion = findViewById(R.id.question);
         flashcardAnswer = findViewById(R.id.answer);
+        flashcardQuestion2 = findViewById(R.id.question2);
+        flashcardAnswer2 = findViewById(R.id.answer2);
     }
 
+    // method for save button on main activity
     public void saveCard (View view) {
+        // open a shared preferences editor
         SharedPreferences sharedpreferences = getSharedPreferences(mypreference, Context.MODE_PRIVATE);
         SharedPreferences.Editor editor = sharedpreferences.edit();
+        // put the data from all 4 edit views into the shared preferences
         editor.putString(question, q);
         editor.putString(answer, a);
+        editor.putString(question2, q2);
+        editor.putString(answer2, a2);
         editor.commit();
 
         //Create a Toast
@@ -44,10 +61,15 @@ public class MainActivity extends AppCompatActivity {
         toast.show();
     }
 
+    // method to call next activity to view flashcards
     public void viewCard(View view) {
+        // create the intent
         Intent intent = new Intent (this, Flashcard.class);
+        // pass all 4 strings into the intent
         intent.putExtra(ANSWER, flashcardAnswer.getText().toString());
         intent.putExtra(QUESTION, flashcardQuestion.getText().toString());
+        intent.putExtra(ANSWER2, flashcardAnswer2.getText().toString());
+        intent.putExtra(QUESTION2, flashcardQuestion2.getText().toString());
         startActivity(intent);
     }
 
